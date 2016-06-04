bf412d68b3ec0e0238c2bba8ebd4b96048351903
andylamp <andreas.grammenos@gmail.com>
2016-06-04 06:52:06 +0300

    formatting...

--- cgen.h
+++ cgen.h
@@ -4,8 +4,8 @@
 #include "hashtable.h"
 
 /*
-	String streams are handy for using standard C-library
-	functions to produce formatted strings.
+    String streams are handy for using standard C-library
+    functions to produce formatted strings.
 */
 typedef struct sstream {
     char *buffer;
@@ -21,24 +21,18 @@ void ssclose(sstream *S);
 
 
 /*
- 	This function takes the same arguments as printf,
- 	but returns a new string with the output value in it.
+    This function takes the same arguments as printf,
+    but returns a new string with the output value in it.
  */
-char* template(
-const char *pat, ...
-);
+char* template(const char *pat, ...);
 
-/* wraper for cleaning flex and hashtable */
+/* wrapper for cleaning flex and hashtable */
 void flex_closure();
 
-/*
-	This is the function used to report errors in the translation.
-*/
+/* This is the function used to report errors in the translation. */
 void yyerror(char const *pat, ...);
 
-/*
-	This is set to the number of calls to yyerror
- */
+/* This is set to the number of calls to yyerror */
 extern uint32_t yyerror_count;
 
 
@@ -46,7 +40,7 @@ extern uint32_t yyerror_count;
 extern const char *c_prologue;
 
 /*
- Make a C string literal out of a PTUC string literal.
- Return the corrected string (maybe the same as P).
+    Make a C string literal out of a PTUC string literal.
+    Return the corrected string (maybe the same as P).
 */
 char *string_ptuc2c(char *P);
